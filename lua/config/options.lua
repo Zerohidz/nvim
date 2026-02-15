9f1192211c4a56d92cd68563dfd0167f40f97aa9
Zerohidz <dursunzahidkorkmaz@gmail.com>
2026-02-16 01:41:34 +0300

    make timeout len 300ms -> 500ms

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Türkçe klavye textobject fix: ı→i'den sonra İ/Ğ için yeterli süre
+-- Default 300ms, 500ms ile tuş arası 500ms'e kadar olan gecikmeler çalışır
+vim.opt.timeoutlen = 500
