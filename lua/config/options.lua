180c251fa2b4714fb0f8d2d52b80db37bf6f1323
Zerohidz <dursunzahidkorkmaz@gmail.com>
2026-02-20 20:37:59 +0300

    make timeoutlen=3000

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,4 @@ vim.opt.relativenumber = false
 
 -- Türkçe klavye textobject fix: ı→i'den sonra İ/Ğ için yeterli süre
 -- Default 300ms, 500ms ile tuş arası 500ms'e kadar olan gecikmeler çalışır
-vim.opt.timeoutlen = 500
+vim.opt.timeoutlen = 3000
