7fadd61c977d7b5b11b8ee6cdecd372959504e4e
Zerohidz <dursunzahidkorkmaz@gmail.com>
2026-06-05 20:24:42 +0300

    add `neovide configuration` and update `simple-focus` theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,8 @@ vim.opt.relativenumber = false
 -- Türkçe klavye textobject fix: ı→i'den sonra İ/Ğ için yeterli süre
 -- Default 300ms, 500ms ile tuş arası 500ms'e kadar olan gecikmeler çalışır
 vim.opt.timeoutlen = 3000
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaMono Nerd Font:h10"
+  vim.g.neovide_scale_factor = 1.0
+end
