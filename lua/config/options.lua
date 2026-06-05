3f26d8060b77c3fa4ac44f625e772446ff434705
Zerohidz <dursunzahidkorkmaz@gmail.com>
2026-06-05 20:35:22 +0300

    add padding to neovide window

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,4 +10,8 @@ vim.opt.timeoutlen = 3000
 if vim.g.neovide then
   vim.o.guifont = "CaskaydiaMono Nerd Font:h10"
   vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_padding_top = 30
+  vim.g.neovide_padding_bottom = 30
+  vim.g.neovide_padding_left = 30
+  vim.g.neovide_padding_right = 30
 end
